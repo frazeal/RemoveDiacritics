015183dad8c10db649182cd52d1ab5b85864446d
Francisco A. Alves <aaalves.francisco@gmail.com>
2018-05-17 23:14:08 -0300

    final

--- RemoveDiacritics/Program.cs
+++ RemoveDiacritics/Program.cs
@@ -6,7 +6,11 @@ namespace RemoveDiacritics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string texto = "Código é Muito difícil de escrever. Por isso é imporantíssimo fazer muitos exercícios. Faça! Faça! Hoje e amanhã.";
+            string texto2 = "Testando um código que contém caracteres \t\t\tnuméricos e outros: 109808| 098\\ 041?82/0128-=+*&¨94 091";
+            Console.WriteLine(texto.RemoveDiacritics());
+            Console.WriteLine(texto2.RemoveDiacritics2());
+            Console.ReadKey();
         }
     }
 }
---
+++ RemoveDiacritics/StringExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using System.Linq;
+
+namespace RemoveDiacritics
+{
+    public static class StringExtensions
+    {
+        public static string RemoveDiacritics(this string text)
+        {
+            string normalizedString = text.Normalize(NormalizationForm.FormD);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var ch in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(ch);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(ch);
+                }
+            }
+            return sb.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        public static string RemoveDiacritics2(this string str)
+        {
+            if (str == null) return null;
+            var chars = str
+                .Normalize(NormalizationForm.FormD)
+                .ToCharArray()
+                .Where(ch => CharUnicodeInfo.GetUnicodeCategory(ch) != UnicodeCategory.NonSpacingMark)
+                .ToArray();
+
+            return new string(chars).Normalize(NormalizationForm.FormD);
+        }
+
+        public static string RemoveDiacritics3(this string str)
+        {
+            if (str == null) return null;
+            var chars =
+                from c in str.Normalize(NormalizationForm.FormD).ToCharArray()
+                let uc = CharUnicodeInfo.GetUnicodeCategory(c)
+                where uc != UnicodeCategory.NonSpacingMark
+                select c;
+
+            var cleanStr = new string(chars.ToArray()).Normalize(NormalizationForm.FormC);
+
+            return cleanStr;
+        }
+    }
+}
